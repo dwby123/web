be816ddfd7c7117d14a72ebc6d2b60d80195edf4
dwby123 <1830108612@qq.com>
2025-03-15 23:25:33 +0800

    2
    2

--- forest.js
+++
@@ -1,8 +0,0 @@
-function makeChoice() {
-    const nameInput = document.getElementById("nameInput").value;
-    if (nameInput === "金属铜条") {
-        window.location.href = "f1.html"; // 解锁成功，跳转到指定页面
-    } else {
-        alert("必须找到钥匙");
-    }
-}
